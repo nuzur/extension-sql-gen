57a72f60afbaac94ac4aaa238176fee3b5d96f2f
Maykel Farha <1123614+mklfarha@users.noreply.github.com>
2024-11-19 17:39:30 -0600

    remove extension uuid match, moved to sdk

--- server/get_execution.go
+++ server/get_execution.go
@@ -6,8 +6,6 @@ import (
 	"github.com/gofrs/uuid"
 	pb "github.com/nuzur/extension-sdk/idl/gen"
 	sdkmapper "github.com/nuzur/extension-sdk/mapper"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 func (s *server) GetExecution(ctx context.Context, req *pb.GetExecutionRequest) (*pb.GetExecutionResponse, error) {
@@ -16,10 +14,6 @@ func (s *server) GetExecution(ctx context.Context, req *pb.GetExecutionRequest) 
 		return nil, err
 	}
 
-	if exec.ExtensionUuid == s.metadata.Uuid {
-		// TODO build step or final data based on the status
-		return sdkmapper.MapExecutionToGetResponse(exec, nil, nil), nil
-	}
-
-	return nil, status.Errorf(codes.InvalidArgument, "execution not found")
+	// TODO build step or final data based on the status
+	return sdkmapper.MapExecutionToGetResponse(exec, nil, nil), nil
 }
